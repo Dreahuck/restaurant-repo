dbf49ddd29bdc3888c92487cdde6e73a4b3ba49f
Dreahuck <dreahuck@gmail.com>
2023-11-04 23:56:22 +0100

    Update MenuLocalRepository.cs

--- restaurant/Metier/MenuLocalRepository.cs
+++ restaurant/Metier/MenuLocalRepository.cs
@@ -11,7 +11,7 @@ namespace restaurant.Metier
             new Menu("Menu de Janvier", "01/01/2023".ToDateTime(), "31/01/2023".ToDateTime()),
             new Menu("Menu de Mars", "01/03/2023".ToDateTime(), "31/03/2023".ToDateTime()),
             new Menu("Menu de Octobre", "01/10/2023".ToDateTime(), "31/10/2023".ToDateTime()),
-            new Menu("Menu de Novembre", "01/11/2023".ToDateTime(), "30/11/2023".ToDateTime()),
+            new Menu("Menu de Novembre : Spécial Halloween", "01/11/2023".ToDateTime(), "30/11/2023".ToDateTime()),
         };
 
         public override Menu GetMenuOfTheMonth()
